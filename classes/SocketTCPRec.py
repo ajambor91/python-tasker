78269687c4f9210ff44ebd496352c5fdf38a5836
Adam <adam91mko@gmail.com>
2024-03-31 13:54:52 +0200

    merge

--- classes/SocketTCPRec.py
+++ classes/SocketTCPRec.py
@@ -1,15 +1,20 @@
 import socket
+import threading
+
 from app.init import get_config
+import threading
 class SocketTCPRec:
 
     def __init__(self, host):
-
+        self.worker = threading.Thread(target=lambda: self.__start_listening())
         self.host = host
         self.config = get_config()
         self.__set_socket()
-        self.__start_listening()
+        self.__run_socket()
     def stop_listening(self):
         self.server_socket.close()
+    def __run_socket(self):
+        self.worker.start()
     def __set_socket(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.config.get('tcpListenPort')))
@@ -18,6 +23,7 @@ class SocketTCPRec:
         while True:
             client_socket, client_address = self.server_socket.accept()
             print(f"Połączenie nawiązane z {client_address}")
+            print(f"Dane: {client_socket.recv(1024).decode()}")
             client_socket.close()
 
 
